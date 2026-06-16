433b96593a94820d554a7af89064feea3039f444
Szymon Wardak <szymonw.2004@wp.pl>
2026-06-16 23:14:38 +0200

    feat: add better proxy functionality by supabase middleware

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,24 +1,34 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { createClient } from './lib/supabase/server'
+import { updateSession } from './lib/supabase/middleware'
 
 const ALLOWED_PREFIXES = ['/auth', '/dashboard']
 
-export function proxy(request: NextRequest) {
-  const { pathname } = request.nextUrl
+export async function proxy(request: NextRequest) {
+  // const { pathname } = request.nextUrl
+  //
+  // const supabase = await createClient()
+  // const { data } = await supabase.auth.getUser()
+  // const user = data.user ?? null
+  //
+  //
+  // if (pathname === '/') {
+  //   return NextResponse.redirect(new URL('/dashboard', request.url))
+  // }
+  //
+  // const isAllowed = ALLOWED_PREFIXES.some(
+  //   (prefix) => pathname === prefix || pathname.startsWith(prefix + '/')
+  // )
+  //
+  // if (!isAllowed) {
+  //   return NextResponse.redirect(new URL('/auth', request.url))
+  // }
+  //
+  // return NextResponse.next()
 
-  if (pathname === '/') {
-    return NextResponse.redirect(new URL('/dashboard', request.url))
-  }
 
-  const isAllowed = ALLOWED_PREFIXES.some(
-    (prefix) => pathname === prefix || pathname.startsWith(prefix + '/')
-  )
-
-  if (!isAllowed) {
-    return NextResponse.redirect(new URL('/auth', request.url))
-  }
-
-  return NextResponse.next()
+  return await updateSession(request)
 }
 
 export const config = {
